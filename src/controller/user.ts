6346596636493e16dee63e212819cce08677803c
Nnenna <nnennahumphrey.nh@gmail.com>
2024-02-21 09:18:45 +0100

    mybranch updated. you can now add the url to the .env file

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "../entity/user";
-import { Course } from '../entity/course';
 import { AppDataSource } from "../database/data-source";
 import jwt from "jsonwebtoken";
-import { verify } from 'jsonwebtoken';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -52,56 +50,6 @@ export const loginUser = async (req: Request, res: Response) => {
   }
 };
 
-export const courseController = async (req: Request, res: Response) => {
-  try {
-    // Destructure required properties from req.body
-    const { courseType, studyMode, courseSearch, entryYear, entryMonth } = req.body;
-
-    // Get the token from the request headers
-    const token = req.headers.authorization;
-
-    // Check if the token exists
-    if (!token) {
-      return res.status(401).json({ error: 'Authorization token is missing' });
-    }
-
-    // Verify the token and extract the user ID
-    const decodedToken = verify(token, 'your_secret_key');
-    const userId = (decodedToken as any).userId; // Assuming the user ID is stored in the token as 'userId'
-
-    // Get repository for User model
-    const userRepository = AppDataSource.getRepository(User);
-    // Find the user by ID
-    const user = await userRepository.findOne(userId);
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    // Create a new instance of Course with provided arguments
-    const course = new Course(
-      courseType,
-      studyMode,
-      courseSearch,
-      entryYear,
-      entryMonth,
-      user // Attach the user to the course
-    );
-
-    // Get repository for Course model
-    const courseRepository = AppDataSource.getRepository(Course);
-    // Save the course to the database
-    await courseRepository.save(course);
-
-    // Return success response with status code 201
-    return res.status(201).json({ message: 'Course added successfully', course });
-  } catch (error) {
-    // Log the error
-    console.error('Error adding course:', error);
-    // Return internal server error response with status code 500
-    return res.status(500).json({ error: 'unsuccessful' });
-  }
-};
 
 
 
