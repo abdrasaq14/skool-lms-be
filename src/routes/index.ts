3997b0e9fc318c6f9df335b22433702ba9b0e5a6
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-03-07 04:44:33 +0100

    conflict resolved

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { singleDownload } from '../controller/admin';
 const router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res,) {
   res.send('I am connected to the PostgresSql database succefully');
 });
+
+router.get('/download-pdf', singleDownload);
 
 export default router;
