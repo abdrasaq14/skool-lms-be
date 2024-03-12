7cb2eb9454baae24b64a8aa3d147cae1b9816ef0
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-03-12 14:20:00 +0100

    resolved duplicate single download function

--- src/controller/admin.ts
+++ src/controller/admin.ts
@@ -2,22 +2,6 @@
 import puppeteer from 'puppeteer';
 import { Request, Response } from 'express';
 
-
-export const singleDownload = async (req: Request, res: Response) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:5173/admin/dashboard/application-view', { waitUntil: 'networkidle0' });
-  
-    const pdfBuffer = await page.pdf({ format: 'Tabloid' });
-  
-    await browser.close();
-  
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename=download.pdf');
-    res.send(pdfBuffer);
-  }
-
-
   export const singleDownload = async (req: Request, res: Response) => {
     const id = req.params.id;
 
@@ -34,4 +18,4 @@ export const singleDownload = async (req: Request, res: Response) => {
 
     res.send(htmlContent);
   }
-};
+
